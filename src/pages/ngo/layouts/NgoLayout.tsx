2c892fbae6d0a4f3f3f7b160115b816fee08ae6a
zaid <mohdzaidshaikh4026@gmail.com>
2025-04-10 22:29:25 +0530

    height adjust

--- src/pages/ngo/layouts/NgoLayout.tsx
+++ src/pages/ngo/layouts/NgoLayout.tsx
@@ -13,7 +13,7 @@ const NgoLayout = () => {
         <SidebarProvider>
             <NgoSidebar />
             <SidebarInset>
-                <header className="group-has-data-[collapsible=icon]/sidebar-wrapper:h-16 flex h-16 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear">
+                <header className="group-has-data-[collapsible=icon]/sidebar-wrapper:h-16 flex h-16 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear sticky top-0 bg-white">
                     <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
                         <SidebarTrigger className="-ml-1" />
                         <Separator
@@ -23,7 +23,7 @@ const NgoLayout = () => {
                         <h1 className="text-base font-medium">Documents</h1>
                     </div>
                 </header>
-                <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+                <div className="flex flex-1 flex-col gap-4 pt-0 bg-amber-100 ">
                     <Outlet />
                 </div>
             </SidebarInset>
